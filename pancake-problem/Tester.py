b37e3622f4d22c2f86c97fe19f12b91f5ebc9523
Muhammad Waseem <wasymshykh@gmail.com>
2019-10-31 13:40:05 +0500

    pancake-problem dfs code

--- pancake-problem/Tester.py
+++ pancake-problem/Tester.py
@@ -1,6 +1,9 @@
 from main_classes.PancakeProblem import PancakeProblem
-from bfs_classes.Search import Search
+from bfs_classes.Search import Search as BFS_Search
 from bfs_classes.BreadthFirstSearchStrategy import BreadthFirstSearchStrategy
+from dfs_classes.Search import Search as DFS_Search
+from dfs_classes.DepthFirstSearchStrategy import DepthFirstSearchStrategy
+
 
 # initial = [1, 3, 2, 4, 5]
 # goal = [1, 2, 3, 4, 5]
@@ -12,11 +15,24 @@ initial = [1, 3, 2]
 goal = [1, 2, 3]
 
 pp = PancakeProblem(initial, goal)
+
+'''
+# BFS
 bfs = BreadthFirstSearchStrategy()
-bf_search = Search(pp, bfs)
+bf_search = BFS_Search(pp, bfs)
 
 bfs_result = bf_search.start_bfs()
 bf_search.print_result(bfs_result)
+'''
+
+# '''
+# DFS
+dfs = DepthFirstSearchStrategy()
+df_search = DFS_Search(pp, dfs)
+
+dfs_result = df_search.start_dfs()
+df_search.print_result(dfs_result)
+# '''
 
 
 
---
+++ pancake-problem/dfs_classes/Search.py
@@ -0,0 +1,51 @@
+from main_classes.PancakeProblem import PancakeProblem
+from bfs_classes.Node import Node
+from dfs_classes.DepthFirstSearchStrategy import DepthFirstSearchStrategy
+
+
+class Search:
+    search_problem: PancakeProblem
+    search_strategy: DepthFirstSearchStrategy
+
+    def __init__(self, search_problem, search_strategy):
+        self.search_problem = search_problem
+        self.search_strategy = search_strategy
+        self.explored = 0
+
+    def start_dfs(self):
+
+        node = Node(self.search_problem.initial_state(), None, 0, 0, '')
+        self.search_strategy.add_node(node)
+
+        duplicate_map = {node.state.string_rep(): node.state.string_rep()}
+
+        result = None
+
+        while not self.search_strategy.is_empty():
+            current_node = self.search_strategy.remove_node()
+
+            if self.search_problem.is_goal(current_node.state.get_current_state()):
+                result = current_node
+                break
+            else:
+                self.explored += 1
+
+            next_moves = self.search_problem.successor_function(current_node.state)
+
+            for next_state in next_moves:
+                if next_state.string_rep() not in duplicate_map:
+                    new_node = Node(next_state, current_node, current_node.depth + 1,
+                                    current_node.cost + next_state.cost, next_state.action)
+                    self.search_strategy.add_node(new_node)
+                    duplicate_map[new_node.state.string_rep()] = new_node.state.string_rep()
+
+        return result
+
+    def print_result(self, result: Node):
+        if result.parent is None:
+            print("Nodes Explored " + str(self.explored))
+            print("Initial State: %s" % result.state.get_current_state())
+            return
+        self.print_result(result.parent)
+        print("Perform: action[%s] - new state[%s] - cost[%d]" % (
+            result.action, result.state.get_current_state(), result.cost))
